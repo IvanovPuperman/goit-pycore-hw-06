fb6013e2d494d8aafc71c9ad755c8665f07dad75
Volodymyr Ivanov <64498269+IvanovPuperman@users.noreply.github.com>
2026-03-08 14:40:33 +0000

    Task02 done

---
+++ Homework_task02.py
@@ -0,0 +1,17 @@
+def get_cats_info(path):
+    cats = []
+    
+    try:
+        with open(path, "r", encoding="utf-8") as file:
+            for line in file:
+                cat_id, name, age = line.strip().split(",")
+                cats.append({
+                    "id": cat_id,
+                    "name": name,
+                    "age": age
+                })
+        return cats
+
+    except FileNotFoundError:
+        print("Файл не знайдено.")
+        return []
